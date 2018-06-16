3e8684dcc21949beed62540af52fd1469127fdc1
Dubos <31757943+Duboshi@users.noreply.github.com>
2018-06-16 22:39:09 +0800

    Update 010610解法二.cpp

--- noi.openjudge.cn/010610解法二.cpp
+++ noi.openjudge.cn/010610解法二.cpp
@@ -8,55 +8,53 @@
 
 using namespace std;
 
-int an1[210];
-int an2[210];
-char szLine1[210];
-char szLine2[210];
+int num1[205], num2[205];
+char s1[205], s2[205];
 
 int main()
 {
-	scanf("%s", szLine1);
-	scanf("%s", szLine2); 
+	scanf("%s", s1);
+	scanf("%s", s2); 
 	
 	int i, j;	//数组序号 
 	
 	//将两个数字逆序储存到数组中，个位储存在序号0位置，十位在序号1位置... 
-	int nLen1 = strlen(szLine1);
+	int len1 = strlen(s1);
 	j = 0;
-	for(i=nLen1-1; i>=0; i--)
-		an1[j++] = szLine1[i] - '0';
+	for(i=len1-1; i>=0; i--)
+		num1[j++] = s1[i] - '0';
 		
-	int nLen2 = strlen(szLine2);
+	int len2 = strlen(s2);
 	j = 0;
-	for(i=nLen2-1; i>=0; i--)
-		an2[j++] = szLine2[i] - '0';
+	for(i=len2-1; i>=0; i--)
+		num2[j++] = s2[i] - '0';
 		
-	int len = max(nLen1, nLen2) + 1;
+	int len = max(len1, len2) + 1; //这是结果所需的最多位数
 		
 	//计算和与进位，结果存储在an1中
 	for(i=0; i<len; i++)
 	{
-		an1[i]+=an2[i];
-		if(an1[i]>=10)
+		num1[i]+=num2[i];
+		if(num1[i]>=10)
 		{
-			an1[i]-=10;
-			an1[i+1]++;
+			num1[i]-=10;
+			num1[i+1]++;
 		}
 	}
 	
-	bool bStartOutput = false;
+	bool check = false; //是否可以逐位将数字输出，最开始为false，因为可能开始是0
 	for(i=len; i>=0; i--) //输出 
 	{
-		if(bStartOutput) //3. 首个非0值输出之后，bStartOutput变为真，正常输出 
-			printf("%d", an1[i]);
+		if(check) //3. 首个非0值输出之后，bStartOutput变为真，正常输出 
+			printf("%d", num1[i]);
 		else 			//1. 从这里开始 
-			if(an1[i])	//2. 遇到首个非0值才开始输出 
+			if(num1[i])	//2. 遇到首个非0值才开始输出 
 			{
-				printf("%d", an1[i]);
-				bStartOutput = true;
+				printf("%d", num1[i]);
+				check = true;
 			}
 	}
-	if(!bStartOutput) printf("%d", 0); //输入0， 0，则输出0 
+	if(!check) printf("%d", 0); //输入0， 0，则输出0 
 		
 	return 0;
 }
